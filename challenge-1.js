7ff6938c66b7fd0e1f8eae081e8d8d77c2d9ad32
Kgotso Makhalimele <kgotsom@protonmail.com>
2023-04-01 19:44:23 +0200

    first commit

---
+++ challenge-1.js
@@ -0,0 +1,41 @@
+FREE_WARNING = 'Free shipping only applies to single customer orders'
+BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+NONE_SELECTED = '0'
+
+if (location === RSA) {
+    shipping === 400 && currency === 'R' 
+}
+
+if (location === NAM) {
+    shipping === 600 && currency === '$'
+} 
+
+if (location === 'NK') {
+    console.log(BANNED_WARNING)
+}
+
+else {
+    shipping === 800 && currency === 'USD'
+}
+
+shoes = 300 * 1
+toys - 100 * 5
+shirts = 150 * 'NONE_SELECTED'
+batteries = 35 * 2
+pens = 5 * 'NONE_SELECTED' 
+
+if (shoes + batteries + pens + shirts > R1000 || shoes + batteries + pens + shirts >= $60) {
+    
+	if (location = NAM && customers < 2) {
+			if (location = RSA)
+		    shipping = 0 || calcShipping
+		}
+}
+
+if (shipping === 0 && customers !== 1) { 
+    console.log(WARNING) 
+}
+
+customers = '1'
+const location = 'RSA'
+currency = null
