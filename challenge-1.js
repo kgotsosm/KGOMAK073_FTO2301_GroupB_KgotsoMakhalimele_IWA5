acf1b581c74fed42bcafeb0308bd39e440be6786
Kgotso Makhalimele <kgotsom@protonmail.com>
2023-04-03 11:56:08 +0200

    updated challenge-1.js

--- challenge-1.js
+++ challenge-1.js
@@ -1,41 +1,41 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = 0;
 
-if (location === RSA) {
-    shipping === 400 && currency === 'R' 
+let customers = 1;
+let location = 'RSA';
+let currency = null;
+
+let shipping;
+let shoes = 300;
+let toys = 100 * 5;
+let shirts = 150 * NONE_SELECTED;
+let batteries = 35 * 2;
+let pens = 5 * NONE_SELECTED;
+
+if (location === 'RSA') {
+    shipping = 400;
+    currency = 'R';
+} else if (location === 'NAM') {
+    shipping = 600;
+    currency = '$';
+} else if (location === 'NK') {
+    console.log(BANNED_WARNING);
+} else {
+    shipping = 800;
+    currency = 'USD';
 }
 
-if (location === NAM) {
-    shipping === 600 && currency === '$'
-} 
+const cost = shoes + toys + shirts + batteries + pens;
 
-if (location === 'NK') {
-    console.log(BANNED_WARNING)
+if (customers === 1) {
+    if ((location === 'NAM' && cost >= 60) || (location === 'RSA' && cost >= 1000)) {
+        shipping = 0;
+    } 
+} else if (customers > 1) {
+    console.log(FREE_WARNING);
 }
 
-else {
-    shipping === 800 && currency === 'USD'
-}
+let price = currency + (cost + shipping)
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries = 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-if (shoes + batteries + pens + shirts > R1000 || shoes + batteries + pens + shirts >= $60) {
-    
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-}
-
-if (shipping === 0 && customers !== 1) { 
-    console.log(WARNING) 
-}
-
-customers = '1'
-const location = 'RSA'
-currency = null
+console.log('Price: ', price)
